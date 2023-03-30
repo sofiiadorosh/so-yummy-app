20c574bd7ec656bbe834a3ee6759168202ece219
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-03-30 13:18:16 +0300

    feat: add files structure

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+};
---
+++ src/components/SharedLayout/SharedLayout.js
@@ -0,0 +1,3 @@
+export const SharedLayout = () => {
+  return <div>SharedLayout</div>;
+};

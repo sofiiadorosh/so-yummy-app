4084541cbe2fd6cbcd7784b297771903d516a280
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-04-03 18:05:44 +0300

    redux, apisearch

---
+++ src/services/api/search.js
@@ -0,0 +1,33 @@
+export const getSearchByTitle = async (
+  query,
+  page = 1,
+  limit = 12,
+  sort = 'popular'
+) => {
+  try {
+    const { data } = await axios.get(
+      `/search?title=${query.trim()}&page=${page}&limit=${limit}&sort=${sort}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};
+
+export const getSearchByIngredients = async (
+  query,
+  page = 1,
+  limit = 12,
+  sort = 'popular'
+) => {
+  try {
+    const { data } = await axios.get(
+      `/search?ingredient=${query.trim()}&page=${page}&limit=${limit}&sort=${sort}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+};

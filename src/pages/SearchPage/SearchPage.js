3cc49915ae518c58fa3c61025f6aa96f38caba42
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-04-03 18:40:40 +0300

    copy-past-from-categories ^^

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,3 +1,23 @@
+import { MainPageTitle } from 'components/MainPageTitle';
+import { SearchBar } from 'components/SearchBar';
+import { SearchedRecipesList } from 'components/SearchedRecipesList';
+import { Square } from 'components/Square';
+
+import { CategoriesSection, Container, Title } from './SearchPage.slyled';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CategoriesSection>
+        <Container>
+          <Square />
+          <Title>
+            <MainPageTitle title="Search" />
+          </Title>
+          <SearchBar />
+          <SearchedRecipesList />
+        </Container>
+      </CategoriesSection>
+    </>
+  );
 };

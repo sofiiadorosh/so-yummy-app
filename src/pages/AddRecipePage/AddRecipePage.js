75144797bc4b59df7169adee56bf65ab5d12ef0c
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-04-06 19:26:47 +0300

    styles

--- src/pages/AddRecipePage/AddRecipePage.js
+++ src/pages/AddRecipePage/AddRecipePage.js
@@ -1,20 +1,8 @@
 
-import { MainPageTitle } from 'components/MainPageTitle';
-import { Square } from 'components/Square';
-import {AddRecipeForm} from "../../components/AddRecipeForm/AddRecipeForm"
 
-import { AddRecipeSection, Container, Title } from './AddRecipePage.styled';
+import {AddRecipeForm} from "../../components/AddRecipeForm/AddRecipeForm"
 
 export const AddRecipePage = () => {
   return  ( 
-  <> 
-  <AddRecipeSection>
-<Container>
-  <Square />  
-  <Title>
-  <MainPageTitle title="Add recipe" />
   <AddRecipeForm/>
-  </Title>
-</Container>
-</AddRecipeSection></>
 );};

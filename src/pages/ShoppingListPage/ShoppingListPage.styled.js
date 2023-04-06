6c2bf2eb2ccfedd12223f0ba4315e6df071e3037
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-04-06 19:35:48 +0300

    fix head

--- src/pages/ShoppingListPage/ShoppingListPage.styled.js
+++ src/pages/ShoppingListPage/ShoppingListPage.styled.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const ShoppingListSection = styled.section`
-  padding: 50px 0 100px;
+  padding: 114px 0 100px;
 
   @media screen and (min-width: 768px) {
-    padding: 72px 0 200px;
+    padding: 136px 0 200px;
   }
 
   @media screen and (min-width: 1440px) {
-    padding-top: 100px;
+    padding-top: 164px;
   }
 `
 

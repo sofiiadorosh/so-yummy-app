65e32fe593fcba961cc608a9f42f6d50a4338c14
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-04-09 15:55:16 +0300

    +-

--- src/components/RecipePageHero/RecipePageHero.js
+++ src/components/RecipePageHero/RecipePageHero.js
@@ -1,4 +1,8 @@
 import { AiOutlineClockCircle } from 'react-icons/ai';
+import React, { useState } from 'react';
+import Notiflix from 'notiflix';
+
+import { addToFavorite, deleteFromFavorite } from 'services/soyummyAPI';
 
 import {
   RecipeHeroWrapper,
@@ -10,13 +14,61 @@ import {
   RecipeTime,
 } from './RecipePageHero.styled';
 
-export const RecipePageHero = ({ item: { title, description, time } }) => {
+export const RecipePageHero = ({ title, description, time, id }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const addFavorite = () => {
+    addToFavorite(id)
+      .then(res => {
+        console.log(res);
+        setIsFavorite(true);
+        Notiflix.Notify.warning(`Added to favorite!`);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  const deleteFavorite = () => {
+    deleteFromFavorite(id)
+      .then(res => {
+        console.log(res);
+        setIsFavorite(false);
+        Notiflix.Notify.warning(`Removed from favorite!`);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
   return (
     <RecipeHeroWrapper>
       <Container>
         <RecipeTitle>{title}</RecipeTitle>
         <RecipeDescription>{description}</RecipeDescription>
-        <RecipeAddToFavotite>Add to favorite recipes</RecipeAddToFavotite>
+
+        {isFavorite === false && (
+          <RecipeAddToFavotite
+            type="button"
+            onClick={() => {
+              addFavorite();
+            }}
+          >
+            Add to favorite recipes
+          </RecipeAddToFavotite>
+        )}
+
+        {isFavorite === true && (
+          <RecipeAddToFavotite
+            type="button"
+            onClick={() => {
+              deleteFavorite();
+            }}
+          >
+            Delete favorite recipes
+          </RecipeAddToFavotite>
+        )}
+
         <RecipeTimer>
           <AiOutlineClockCircle />
           <RecipeTime>{time}min</RecipeTime>

845b3351a3ff71ecd123a5c9eb1aed56f575244c
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-04-10 16:04:47 +0200

    deleting tokens from ls when logout

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -10,13 +10,20 @@ const instance = axios.create({
   baseURL: 'https://so-yummy-app-backend.onrender.com/api/',
 });
 
-const setToken = token => {
-  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
+const setToken = (token) => {
+    const accessToken = localStorage.getItem("accessToken");
+     instance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+}; 
 
 const clearAuthHeader = () => {
   instance.defaults.headers.common.Authorization = '';
 };
+
+// instance.interceptors.request.use(config => {
+//   const accessToken = localStorage.getItem("accessToken");
+//   config.headers.common.Authorization = `Bearer ${accessToken}`
+//   return config;
+// })
 
 instance.interceptors.response.use(
   response => response,
@@ -61,7 +68,7 @@ export const login = createAsyncThunk(
       const { data } = await instance.post('/users/login', credentials);
       setToken(data.accessToken);
       localStorage.setItem('refreshToken', data.refreshToken);
-      localStorage.getItem('accessToken', data.accessToken);
+      localStorage.setItem('accessToken', data.accessToken);
       Notify.success(`Welcome back, ${data.user.name}`);
       return data;
     } catch (error) {
@@ -75,6 +82,8 @@ export const logout = createAsyncThunk('users/logout', async (_, thunkAPI) => {
   try {
     await instance.post('/users/logout');
     clearAuthHeader();
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('accessToken');
     Notify.success('You successfully logged out! Hope to see you soon again!');
     return;
   } catch (error) {
@@ -104,7 +113,7 @@ export const logout = createAsyncThunk('users/logout', async (_, thunkAPI) => {
 export const getCurrentUser = createAsyncThunk(
   'auth/currentUser',
   async (_, thunkAPI) => {
-    const persistedAccessToken = localStorage.setItem('accessToken');
+    const persistedAccessToken = localStorage.getItem('accessToken');
     if (!persistedAccessToken) {
       return thunkAPI.rejectWithValue();
     }
@@ -131,3 +140,6 @@ export const updateUserInfo = createAsyncThunk(
     }
   }
 );
+
+
+export default instance;
